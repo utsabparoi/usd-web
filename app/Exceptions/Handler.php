1f7cfb920454d9d3a2d9188e17aaafd270c53ad9
AKASH-SmartSoftwareLtd <akash.smartsoftware@yahoo.com>
2023-04-06 09:02:56 +0600

    add

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,11 @@ class Handler extends ExceptionHandler
             //
         });
     }
+//    public function render($request, Throwable $e){
+//        if($request->is('api/*')){
+//            return response()->json([
+//                'message' => 'Record Not Found'
+//            ],404);
+//        }
+//    }
 }
