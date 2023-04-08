5b6f1fc49445129a3b387b229ef727366e67a7e3
AKASH-SmartSoftwareLtd <akash.smartsoftware@yahoo.com>
2023-04-08 08:56:07 +0600

    add

--- resources/views/admin/investor/index.blade.php
+++ resources/views/admin/investor/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.backend.app')
 @section('title', 'Investor Information')
-
+@push('css')
+    <link rel="stylesheet" href="{{ asset('assets\css\customisedButton.css') }}" />
+@endpush
 @section('content')
 
 <div class="main-content-inner">
@@ -47,45 +49,41 @@
                                                 <td> 0 </td>
                                                 <td> 0 </td>
                                                 <td>
-                                                    @if($investor->payment_image)
+                                                    @if($investor->payment_image || $investor->transaction_id)
                                                         <button class="btn btn-success" style="border: none !important;"
                                                                 id="paymentView"
-                                                                data-id="{{ $investor->id }}"
                                                                 data-name="{{ $investor->name }}"
+                                                                data-image="{{ $investor->payment_image }}"
+                                                                data-transaction="{{ $investor->transaction_id }}"
                                                                 onclick="viewPayment(this)">bkash</button>
                                                     @else
                                                         <button class="btn btn-warning" style="border: none !important;">Wallet</button>
                                                     @endif
-                                                        <!-- The Modal -->
+                                                    <!-- The Modal -->
                                                         <div id="myModal" class="modal">
                                                             <!-- Modal content -->
-                                                            <div class="modal-content" style="width:30%; margin-left:350px">
+                                                            <div class="modal-content">
                                                                 <div class="modal-header">
                                                                     <span class="close">&times;</span>
-                                                                    <div class="campName" id="campName"><h3>Payment Information</h3></div>
+                                                                    <div class="campName text-center">Payment Information</div>
                                                                 </div>
                                                                 <br>
                                                                 <div class="modal-body">
                                                                     <div class="row">
-                                                                        <div class="col-lg-12">
-                                                                            <table id="storeList">
-                                                                                <div class="cardmodel">
-                                                                                    <img src="{{ asset($investor->payment_image) }}" alt="Payment-image" style="width:30%">
-                                                                                   <div class="title">
-                                                                                    <h2>{{ $investor->name }}</h2>
-                                                                                    <p><i class="fa fa-credit-card" aria-hidden="true"></i>Transaction No: {{ $investor->transaction_id }}</p>
-                                                                                    <p><i class="fa fa-envelope" aria-hidden="true"></i> {{ $investor->email }}</p>
-                                                                                    <p> <i class="fa fa-phone-square" aria-hidden="true"></i> {{ $investor->mobile }}</p>
-                                                                                   </div>
-                                                                                  </div>
+                                                                        <div class="col-10">
+                                                                            <div align="center">
+                                                                            <table>
+                                                                                <tbody>
+                                                                                <tr class="text-center"><td><img src="{{ asset($investor->payment_image) }}" width="330" height="auto" id="paymentImage"></td></tr>
+                                                                                <tr class="text-center"><td><span id="campName" style="font-size: 20px; color: #656565; font-family: 'Droid Sans';">{{ $investor->name }}</span></td></tr>
+                                                                                <tr class="text-center"><td style="font-size: 18px; color: #656565; font-family: Bahnschrift;">Transaction ID: <span id="transactionId">{{ $investor->transaction_id }}</span></td></tr>
+                                                                                </tbody>
                                                                             </table>
+                                                                            </div>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                                 <br>
-                                                                <div class="modal-footer">
-
-                                                                </div>
                                                             </div>
                                                         </div>
                                                         <!-- Modal work end-->
@@ -126,73 +124,25 @@
 
 <script>
     function viewPayment(element) {
-        // Get the modal
         var modal = document.getElementById("myModal");
-
-        // Get the button that opens the modal
         var callButton = document.getElementById("paymentView");
-
-        // Get the <span> element that closes the modal
         var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal
-        /*callButton.onclick = function() {
-            modal.style.display = "block";
-        }*/
         modal.style.display = "block";
-
-        // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
             modal.style.display = "none";
         }
-
-        // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
             }
         }
-
-        // let camp_id = $(element).attr("data-id");
-        // let camp_name = $(element).attr("data-name");
-        // document.getElementById('campName').innerHTML = camp_name;
-        //
-        // let url = "/camp_store";
-        // let allData = {
-        //     CampId: camp_id
-        // };
-        //
-        // axios.post(url, allData).then(
-        //     function(response) {
-        //         var responseData = response.data;
-        //         var serialNumber = 1;
-        //         $('#storeList').empty();
-        //         $('#storeList').append("" +
-        //             "<tr>\n" +
-        //             "                                                                                    <th width=\"5%\">SL</th>\n" +
-        //             "                                                                                    <th width=\"40%\">Store</th>\n" +
-        //             "                                                                                    <th width=\"40%\">Store Man</th>\n" +
-        //             "                                                                                </tr>");
-        //         for (let i = 0; i < responseData.length; i++) {
-        //             $('#storeList').append("" +
-        //                 "<tr align=\"left\">\n" +
-        //                 "                                                                                    <td>" +
-        //                 serialNumber + "</td>\n" +
-        //                 "                                                                                    <td>" +
-        //                 responseData[i].name + "</td>\n" +
-        //                 "                                                                                    <td>" +
-        //                 response.data[i].store_man + "</td>\n" +
-        //                 "                                                                                </tr>");
-        //             serialNumber++;
-        //         }
-        //     }
-        // ).catch(
-        //     function(error) {
-        //         alert("Error...try again");
-        //     }
-        // )
+        let investor_name = $(element).attr("data-name");
+        let payment_image = $(element).attr("data-image");
+        let payment_transaction = $(element).attr("data-transaction");
+        document.getElementById('campName').innerHTML = investor_name;
+        document.getElementById('paymentImage').src = payment_image;
+        document.getElementById('transactionId').innerHTML = payment_transaction;
     }
-
 </script>
 @endpush
 @endsection
